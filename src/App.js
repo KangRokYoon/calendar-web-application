5a33d1f38d8dc31b8b4bd4cbf1a41abd2056700e
KangRok Yoon <ykr0919@naver.com>
2023-07-19 18:31:35 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
+import { useCallback, useRef, useState } from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+ 
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일해 보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false,
+    },
+  ]);
 
-import './App.css';
+  // 고윳값으로 사용될 id
+  // ref를 사용하여 변수 담기
 
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked:false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1; // nextId 1씩 더하기
+    },
+    [todos],
   );
-}
 
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert}/>
+      <TodoList todos={todos}/>
+    </TodoTemplate>
+  );
+};
+ 
 export default App;
